8061e4ab6b15ebc7e1803d26cdb8b101ec64fe24
Prashant <patiprashantanil@gmail.com>
2024-07-20 15:40:10 +0530

    Header page is updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,22 @@ import About from './pages/About.jsx'
 import SignIn from './pages/SignIn.jsx'
 import SignUp from './pages/SignUp.jsx'
 import Dashboard from './pages/Dashboard.jsx'
+import Projects from './pages/Projects.jsx'
+import Header from './components/Header.jsx'
 
 
 function App() {
   return (
 
      <BrowserRouter>
+     <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/sign-in" element={<SignIn/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>4
+      <Route path='project' element={<Projects/>}/>
 
 
     </Routes>
