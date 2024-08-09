75c823784f011e1f33e0f1126795aef04a84c92b
Prashant <patiprashantanil@gmail.com>
2024-08-10 01:10:05 +0530

    sign up page is created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,28 +1,33 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res) =>{
-    const{username,email,password} = req.body;
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
+  if (
+    !username ||
+    !password ||
+    !email ||
+    username === "" ||
+    password === "" ||
+    email === ""
+  ) {
+    next(errorHandler(400, "ALl fields are required"));
+  }
 
-if(!username || !password || !email || username === '' || password === '' || email === ''){
-    return res.status(400).json({message: 'All fields are requred'});
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-}
-
-const hashedPassword = bcryptjs.hashSync(password , 10)
-
-const newUser = new User({
+  const newUser = new User({
     username,
     email,
-    password:hashedPassword});
+    password: hashedPassword,
+  });
 
-   try {
+  try {
     await newUser.save();
-    res.json('Signup Successful..!!');
-   } catch (error) {
-    res.status(500).json({message:error.message})
-    
-   }
-    
-}
+    res.json("Signup Successful..!!");
+  } catch (error) {
+    next(error);
+  }
+};
